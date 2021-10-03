a8694651baf19b53daaecd13ac94a40e2660920c
Vasanth <vasanth4real@gmail.com>
2021-10-03 12:57:33 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int range=8;
+		int firstnum=0;
+		int secondnum=1;
+		int sum;
+		System.out.println(firstnum);
+	
+	
+	 for(int i=1;i<=range;i++) {
+		sum=firstnum+secondnum;
+	firstnum=secondnum;
+	  secondnum=sum;
+	  System.out.println(sum);
+	 
+	}
+}
+	 
+
+}
---
+++ PositiveToNegative.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class PositiveToNegative {
+
+		public static void main(String[] args) {
+			int i=-34;
+			int positivenum = ~(i-1);
+			System.out.println(positivenum);
+		}
+}
